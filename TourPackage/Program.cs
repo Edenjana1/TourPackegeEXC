74558b5a829062d0964b34d8ac469c47129eca7b
User <User@WX1129710>
2024-11-24 11:48:16 +0200

    Add project files.

---
+++ TourPackage/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Main;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+     // TourPackage[] packeges = new TourPackage[3];
+    // packeges[0] = TourPackage(1, 20, 30, 15);
+
+
+    }
+    public static int Calculate(TourPackage[] packages)
+    {
+        int count = 0;
+        for (int i = 0; i < packages.Length; i++)
+        {
+            if (packages[i].GetExtra() > 0) count++;
+        }
+        return count;
+    }
+
+    public static int[] Costumers(TourPackage[] packages)
+    {
+        int[] extraID = new int[Calculate(packages)];
+        int i = 0;
+        for (int k = 0; k < packages.Length; k++)
+        {
+            if ((packages[k].GetExtra() > 0)) extraID[i] = packages[k].GetExtra();
+            i++;
+            
+        }
+        return extraID;
+    }
+}
---
+++ TourPackage/TourPackege.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main 
+{
+    
+    internal class TourPackage
+    {
+        private int id;
+        private int price;
+        private int maxTime;
+        private int maxData;
+        private int extra;
+
+        public TourPackage( int id, int price, int maxTime, int maxData)
+        {
+            this.id = id;
+            this.price = price;
+            this.maxTime = maxTime;
+            this.maxData = maxData;
+            this.extra = 0;
+        }
+         public int GetExtra()
+        {
+            return this.extra;
+        }
+
+        public void SetExtra(int usage, int minutes)
+        {
+            int calcExtra = 0;
+            if (minutes > this.maxTime) calcExtra = minutes - this.maxTime;
+            if (usage > this.maxData) calcExtra += 2 * (usage - this.maxData);
+        }
+
+
+
+
+
+    }
+}
